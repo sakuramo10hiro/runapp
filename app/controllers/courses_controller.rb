764a9e0da84e43c9c9ea67067df931604379ed73
櫻本 <sakuramotohiroshi@sakuramotonoMacBook-Air.local>
2022-12-07 18:14:57 +0900

    モデル/コントローラーの作成

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,33 @@
+class CoursesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def course_params
+    params.require(:list).permit(:course_name, :course_description, :public, :course_image, :distance, :road_type, :toilet, :undulation, :signal, :course_id)
+  end
+
+end
