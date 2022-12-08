2bd4fd51eaf8d464c700c05c15daf81efb19afef
櫻本 <sakuramotohiroshi@sakuramotonoMacBook-Air.local>
2022-12-08 16:21:59 +0900

    ヘッダー・フッターの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get "home" => "courses#show"
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  get '/users/sign_out' => 'devise/sessions#destroy'
   resources :courses, only: [:new, :create, :edit, :update, :destroy]
 end
