1764f5901c44a2e93352d0162b21947cb7df823c
櫻本 <sakuramotohiroshi@sakuramotonoMacBook-Air.local>
2022-12-10 13:42:25 +0900

    deviseの各種設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
   get "home" => "courses#show"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  get '/users/sign_out' => 'devise/sessions#destroy'
   get "users/show" => "users#show"
   resources :courses, only: [:new, :create, :edit, :update, :destroy, :show]
   resources :relationships, only: [:create, :destroy]
-
   resources :courses do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create] 
