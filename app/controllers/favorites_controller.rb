64046ef2abd5d41d84a7525aa0090ef766bbc3f3
櫻本 <sakuramotohiroshi@sakuramotonoMacBook-Air.local>
2022-12-23 21:14:54 +0900

    Font Awesomeの修正、いいね機能の修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,7 @@
 class FavoritesController < ApplicationController
   def create
     @course_favorite = Favorite.new(user_id: current_user.id, course_id: params[:course_id])
+    @course = Course.find(params[:course_id]) 
     @course_favorite.save
     redirect_to request.referer
   end
@@ -9,9 +10,5 @@ class FavoritesController < ApplicationController
     @course_favorite = Favorite.find_by(user_id: current_user.id, course_id: params[:course_id])
     @course_favorite.destroy
     redirect_to request.referer
-  end
-
-  def favorited?(user)
-    favorites.where(user_id: user.id).exists?
   end
 end
